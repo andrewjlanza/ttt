75980b5a9912977cd1f3da953e47cce6b31b0e84
Gabe Oleinik <gomusic14@gmail.com>
2018-09-22 15:33:49 -0400

    more css and one win working

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 let clickCount = 0
 let origBoard
-const playerOne = {letter : 'O', value : 1}
-const playerTwo = {letter : 'X', value : -1}
+const playerOne = { letter: 'O', value: 1 }
+const playerTwo = { letter: 'X', value: -1 }
 const winCombos = [
   [a, b, c],
   [d, e, f],
@@ -13,23 +13,28 @@ const winCombos = [
   [c, e, g]
 ]
 
-const cells = document.querySelectorAll(".cell")
+const playerOneWinCondition = () => {
+  if (origBoard.a === 'X' && origBoard.b === 'X' && origBoard.c === 'X') {
+    document.querySelector('.winMessage').textContent = 'PLAYER ONE WINS'
+  }
+}
+const cells = document.querySelectorAll('.cell')
 startGame()
 
-function startGame() {
-  document.querySelector(".endgame").style.display = "none"
+function startGame () {
+  document.querySelector('.endgame').style.display = ''
   origBoard = Array.from(Array(9).keys())
   for (let i = 0; i < cells.length; i++) {
-    cells[i].innerText = ""
-    cells[i].style.removeProperty("background-color")
-    cells[i].addEventListener("click", turnClick, false)
+    cells[i].innerText = ''
+    cells[i].style.removeProperty('background-color')
+    cells[i].addEventListener('click', turnClick, false)
   }
 }
 
-//const playerClick = square => {}
+// const playerClick = square => {}
 
-function turnClick(square) {
-  //turn(square.target.id, playerOne)
+function turnClick (square) {
+  // turn(square.target.id, playerOne)
   if (
     clickCount === 1 ||
     clickCount === 3 ||
@@ -43,42 +48,46 @@ function turnClick(square) {
   }
 }
 
-function turn(squareId, player) {
+const checkWinner = () => {
+  playerOneWinCondition()
+}
+
+function turn (squareId, player) {
   origBoard[squareId] = player
   document.getElementById(squareId).innerText = player
-  let gameWon = checkWin(origBoard, player)
-  if (gameWon) gameOver(gameWon)
+  //   let gameWon = checkWin(origBoard, player)
+  //   if (gameWon) gameOver(gameWon)
   document.getElementById(squareId).removeEventListener('click', turnClick)
+  checkWinner()
 }
 
-function checkWin(board, player) {
-  //
-  let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), [])
-  let gameWon = null
-  for (let [index, win] of winCombos.entries()) {
-    if (win.every(elem => plays.indexOf(elem) > -1)) {
-      gameWon = { index: index, player: player }
-      break
-    }
-  }
-  return gameWon
-}
+// function checkWin (board, player) {
+//   let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), [])
+//   let gameWon = null
+//   for (let [index, win] of winCombos.entries()) {
+//     if (win.every(elem => plays.indexOf(elem) > -1)) {
+//       gameWon = { index: index, player: player }
+//       break
+//     }
+//   }
+//   return gameWon
+// }
 
-function gameOver(gameWon) {
-  for (let index of winCombos[gameWon.index]) {
-    document.getElementById(index).style.backgroundColor =
-      gameWon.player == playerOne ? "blue" : "red"
-  }
-  for (let i = 0; i < cells.length; i++) {
-    cells[i].removeEventListener("click", turnClick, false)
-  }
-}
+// function gameOver (gameWon) {
+//   for (let index of winCombos[gameWon.index]) {
+//     document.getElementById(index).style.backgroundColor =
+//       gameWon.player == playerOne ? 'blue' : 'red'
+//   }
+//   for (let i = 0; i < cells.length; i++) {
+//     cells[i].removeEventListener('click', turnClick, false)
+//   }
+// }
 
 const main = () => {
-  document.querySelector(".sandbox").addEventListener("click", () => {
+  document.querySelector('.sandbox').addEventListener('click', () => {
     clickCount++
     console.log(clickCount)
   })
 }
 
-document.addEventListener("DOMContentLoaded", main)
+document.addEventListener('DOMContentLoaded', main)
