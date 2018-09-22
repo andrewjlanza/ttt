1f1624927e31622be3996e9bfff3e412066ce7f3
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2018-09-22 11:56:18 -0400

    HTML Changed

--- public/main.js
+++ public/main.js
@@ -1,5 +1,28 @@
+let origBoard;
+const Player1 = 'X';
+const Player2 = 'O';
+const winCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2]
+]
+
+
+
+
+
+
+
+
+
+
 const main = () => {
-  
+
 }
 
 document.addEventListener('DOMContentLoaded', main)
